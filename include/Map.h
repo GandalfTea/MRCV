b8e7605bd1f0105ba905a0c9dfdecb68642fe243
root <root@open-mrcv.europe-west2-c.c.proud-amphora-342215.internal>
2022-04-22 15:52:32 +0000

    Working on Map class and integration / Not yet within Kalman Filter

--- include/Map.h
+++ include/Map.h
@@ -5,11 +5,11 @@
 #include <Frame.h>
 #include <Kalman.h>
 
+#define MRCV_DETAILED // hardcode for now
+
 namespace MRCV {
 
-
 typedef unsigned char uchar;
-
 
 
 struct Observation {
@@ -18,11 +18,20 @@ struct Observation {
 
 
 struct Point {
-	cv::Point2f pt;
-	uchar des;
+	cv::Point3f pt;
+	cv::Mat des;
 	std::vector<Observation> observations;
+
+	cv::Point3f global( Point& a) {
+		// express point in global frame
+	}
 };
 
+#if defined MRCV_POWER_SAVING || defined MRCV_SPEEDY
+	#define MAP_MAX_SIZE 100000 * sizeof(Point)*4
+#elif defined MRCV_DETAILED
+	#define MAP_MAX_SIZE 1000000 * sizeof(double)*4
+#endif
 
 
 class Map {
@@ -30,14 +39,26 @@ class Map {
 		Map();
 		~Map();
 
+		void update( cv::Mat pts, cv::Mat des );
+		void update( std::vector<cv::Point3f> pts, std::vector<uchar> des );
 		void update( const Frame& frame );
 		void clear();
 		void draw();
 		void video();
 
+		// Helpers
+		bool contains( uchar a );
+		void add( Point a );
+		int size();
+
 	private:
-		std::vector<Point> points;
+
+	#if defined MRCV_POWER_SAVING || defined MRCV_SPEEDY
+		cv::Mat points;
+	#elif defined MRCV_DETAILED
+		std::vector<Point>* points;
 		std::vector<cv::Mat> poses;
+	#endif
 };
 
 
--- src/Map.cc
+++ src/Map.cc
@@ -3,12 +3,33 @@
 
 namespace MRCV {
 
-Map::Map() {}
+Map::Map() {
+	this->points = new std::vector<Point>;
+}
 
 Map::~Map() {}
 
-Map::update( const Frame& frame ) {}
+void Map::update( cv::Mat pts, cv::Mat des ) {
+	for( int i = 0; i < pts.cols; i++) {
+		//if( !this->contains(des.row(i))) {
+			Point a;
+			cv::Mat temp = pts.col(i);
+			a.pt = cv::Point3f( temp.at<float>(0,0), temp.at<float>(0,1), temp.at<float>(0,2));
+			a.des = des.row(i);
+			this->add( a ); 
+		//}
+	}
+}
+
+bool Map::contains( uchar a ) {}
+
+void Map::add ( Point a ) {
+	this->points->push_back(a);
+}
+
+int Map::size() {
+	return this->points->size();
+}
 
 
 }  // namespace
-#endif
